b11efbeb5c1ea0f3847552e411d243d6d2920ea7
Tahina <to.tahina@gmail.com>
2022-07-16 21:02:42 +0300

    Installed JWT

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Api\PostController;
 
 /*
@@ -19,5 +19,20 @@ use App\Http\Controllers\Api\PostController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+
+Route::group(
+    [
+        'middleware' => 'api',
+        'prefix' => 'auth'
+    ], 
+    
+    function($router) {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::post('/profile', [AuthController::class, 'profile']);
+});
 
 Route::apiResource('posts', PostController::class);
